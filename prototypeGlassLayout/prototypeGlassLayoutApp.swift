3e952baddec86584731820ea7dd843dccbdbd524
Suhail <suhailpsx69@gmail.com>
2022-03-11 09:41:25 +0530

    Initial Commit

---
+++ prototypeGlassLayout/prototypeGlassLayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  prototypeGlassLayoutApp.swift
+//  prototypeGlassLayout
+//
+//  Created by SUHAIL  on 11/03/22.
+//
+
+import SwiftUI
+
+@main
+struct prototypeGlassLayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
